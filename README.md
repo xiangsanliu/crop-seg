57e522414009bf16c580f86554e5d4c859f8c4d4
Chien <xiangjianjian@hotmail.com>
2021-09-14 15:01:51 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# SPECTRAL-SETR
+
+这个仓库用于做分割任务
+
+comming soon~
+
+
---
+++ batch_test_label.sh
@@ -0,0 +1,7 @@
+python test.py -config configs/hybrid_b3_tianchi2_label.py -weight work/models/hybrid_b3_tianchi2_label/2021-09-03T10:37:45.pkl 
+
+python test.py -config configs/hybrid_b4_tianchi2_label.py -weight work/models/hybrid_b4_tianchi2_label/2021-09-05T16:55:03.pkl 
+
+python test.py -config configs/segformer_b3_tianchi2_label.py -weight work/models/segformer_b3_tianchi2_label/2021-09-04T19:20:43.pkl 
+
+python test.py -config configs/segformer_b4_tianchi2_label.py -weight work/models/segformer_b4_tianchi2_label/2021-09-06T23:14:57.pkl 
